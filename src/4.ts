c34bef54d7f4d90a9f2e625af71de67ee4b409ab
Alexandr150983 <alexandr150983@gmail.com>
2023-11-20 23:31:16 +0200

    HW3 TS final

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.floor(Math.random() * 1000);
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Людина зайшла до будинку.");
+    } else {
+      console.log("Двері зачинені.");
+    }
+  }
+}
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинені.");
+    } else {
+      console.log("Ключ не підійшов.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +63,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
